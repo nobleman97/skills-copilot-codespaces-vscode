a6c0e8b40a59411a25c490463dbb47599ed443dd
David Omokhodion <davidstone097@gmail.com>
2023-07-23 09:43:36 +0000

    My second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['JS', 'CSS', 'HTML']
+    };
+    console.log(member.skills);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+    console.log(member.skills[3]);
+}
