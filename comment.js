88a18e70b27b23f5a78c056d993ec4b1591a7d79
David Omokhodion <davidstone097@gmail.com>
2023-07-23 09:59:40 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,20 @@
+// create web server
+
+// import express
+const express = require('express');
+// import path
+const path = require('path');
+// import body-parser
+const bodyParser = require('body-parser');
+// import express-session
+const session = require('express-session');
+// import express-messages
+const flash = require('express-flash');
+// import express-validator
+const expressValidator = require('express-validator');
+// import passport
+const passport = require('passport');
+// import mongoose
+const mongoose = require('mongoose');
+// import config
+const config = require('./config/database');
